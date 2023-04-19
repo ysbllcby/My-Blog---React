e057d4efe52e22e93ef9687a1d4b6474fd179760
ysbllcby <ysabellacubay@gmail.com>
2023-04-19 11:00:04 -0600

    Created app components and blog pages.

---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticlesListPage = () => {
+    return (
+        <h1>This is the Articles List Page.</h1>
+    );
+}
+
+export default ArticlesListPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return (
+        <h1>This is the Homepage.</h1>
+    );
+}
+
+export default HomePage;
