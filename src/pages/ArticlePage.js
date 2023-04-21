994b1df5f0d658fce50ce36341bd790c9ff175cb
ysbllcby <ysabellacubay@gmail.com>
2023-04-21 13:53:50 -0600

    Adding React.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    
+    useEffect(() => {
+        setArticleInfo({ upvotes:3, comments: [] });
+    }, []);
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
 
@@ -13,6 +20,7 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times</p>
         {article.content.map((paragraph,i) => (
             <p key={i}>{paragraph}</p>
         ))}
