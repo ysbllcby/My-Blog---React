8cca8ae88e82cef11eeb98c2ee70dff99a905eac
ysbllcby <ysabellacubay@gmail.com>
2023-04-24 09:55:54 -0600

    Upvotes working.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const { articleId } = useParams();
     
     useEffect(() => {
-        setArticleInfo({ upvotes:3, comments: [] });
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
     }, []);
 
-    const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
 
     if (!article) {
